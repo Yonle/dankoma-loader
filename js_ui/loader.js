4a220af2cf898649ccc9d018eb016ce6fa89d8fe
Yonle <yonle@proton.me>
2026-09-03 21:28:44 +0700

    push memes
    Signed-off-by: Yonle <yonle@proton.me>

--- js_ui/loader.js
+++ js_ui/loader.js
@@ -10,6 +10,11 @@ const samples = [
         danmaku: "https://pcf.waltuh.cyou/danma/dm_vid/9/danma.json.gz"
     },
     {
+        name: "UP↑SIDE↓UP↑SIDE↓DOWN↓ [BV1da411U7PE]",
+        video: "https://pcf.waltuh.cyou/danma/dm_vid/12/vid.mp4",
+        danmaku: "https://pcf.waltuh.cyou/danma/dm_vid/12/danma.json.gz",
+    },
+    {
         name: "緋色月下、狂咲ノ絶 [BV1qx411c772]",
         video: "https://pcf.waltuh.cyou/danma/dm_vid/3/vid.mp4",
         danmaku: "https://pcf.waltuh.cyou/danma/dm_vid/3/danma.json.gz"
