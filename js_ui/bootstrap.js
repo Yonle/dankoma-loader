27f44cab5ea4d9692c15ae6c948c01c4ec3c5368
Yonle <yonle@proton.me>
2026-09-07 03:47:54 +0700

    bump ver again
    Signed-off-by: Yonle <yonle@proton.me>

--- js_ui/bootstrap.js
+++ js_ui/bootstrap.js
@@ -21,7 +21,7 @@ function loadScript(src) {
 async function main() {
     const dankoma = isNW
         ? "../modules/dankoma.js/js/dankoma.js"
-        : "https://cdn.jsdelivr.net/npm/dankoma.js@0.1.4";
+        : "https://cdn.jsdelivr.net/npm/dankoma.js@0.1.5";
 
     await loadScript(dankoma);
     await import("./videocontrol.js");
